a000bbbf22773415d3720357bf91f1f40a0f6ac6
Philisizwe Kuzwayo <cre8storeese@gmail.com>
2023-04-12 15:27:40 +0200

    Added functionality for multiple cities

--- src/script.js
+++ src/script.js
@@ -1,21 +1,25 @@
 function updateTime() {
     //jhb
     let jhb = document.querySelector("#jhb");
+    if (jhb) {
     let jhbDate = jhb.querySelector(".date");
     let jhbTime = jhb.querySelector(".time");
     let jhbTime2 = moment().tz("Africa/Johannesburg");
 
     jhbDate.innerHTML= jhbTime2.format("MMMM Do YYYY");
-    jhbTime.innerHTML= jhbTime2.format("hh:mm:ss [<small>]A[</small>]"); //fix the hTML
+    jhbTime.innerHTML= jhbTime2.format("hh:mm:ss [<small>]A[</small>]");//fix the hTML
+    }
 
     //ldn
     let ldn = document.querySelector("#ldn");
+    if (ldn) {
     let ldnDate = ldn.querySelector(".date");
     let ldnTime = ldn.querySelector(".time");
     let ldnTime2 = moment().tz("Europe/London");
 
     ldnDate.innerHTML= ldnTime2.format("MMMM Do YYYY");
-    ldnTime.innerHTML= ldnTime2.format("hh:mm:ss [<small>]A[</small>]"); //fic HTM
+    ldnTime.innerHTML= ldnTime2.format("hh:mm:ss [<small>]A[</small>]"); //fic HTML
+    }
 };
 
 updateTime();
@@ -23,15 +27,16 @@ setInterval(updateTime, 1000)
 
 function updateCity(event) {
     let cityTZ = event.target.value;
+    let cityName = cityTZ.replace("_", " ").split("/")[1];
     let cityTime = moment(). tz(cityTZ);
     let city = document.querySelector("#cities")
-    city = `<div class="place" id="jhb">
+    city.innerHTML = `<div class="place" id="jhb">
         <div>
-            <h2>Johannesburg</h2>
-            <div class="date"></div>
+            <h2>${cityName}</h2> 
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
         
-        <div class="time"></div>
+        <div class="time">${cityTime.format("hh:mm:ss")} <small>${cityTime.format("A")}</small></div>
         </div>`
 }
 
